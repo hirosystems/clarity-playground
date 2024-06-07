0754acc96a6943a7f38bf24044d0c2e392418d49
Hugo Caillard <911307+hugocaillard@users.noreply.github.com>
2024-06-07 18:19:32 +0200

    feat: simpler example contract

--- src/scripts/samples/contract.js
+++ src/scripts/samples/contract.js
@@ -1,7 +1,4 @@
 export const counter = `\
-(define-constant ERR_COUNT_MUST_BE_POSITIVE (err u1001))
-(define-constant ERR_BLOCK_NOT_FOUND (err u1003))
-
 (define-data-var count uint u0)
 (define-data-var contract-owner principal tx-sender)
 (define-data-var cost uint u10)
@@ -10,24 +7,10 @@ export const counter = `\
   (var-get count)
 )
 
-(define-read-only (get-count-at-block (block uint))
-  (ok (at-block
-    (unwrap! (get-block-info? id-header-hash block) ERR_BLOCK_NOT_FOUND)
-    (var-get count)
-  ))
-)
-
 (define-public (increment)
   (begin
     (try! (stx-transfer? (var-get cost) tx-sender (var-get contract-owner)))
     (ok (var-set count (+ (var-get count) u1)))
-  )
-)
-
-(define-public (decrement)
-  (let ((current-count (var-get count)))
-    (asserts! (> current-count u0) ERR_COUNT_MUST_BE_POSITIVE)
-    (ok (var-set count (- current-count u1)))
   )
 )
 `;
